39e14df5896dfad66bb86ab88bf0c2dce7a662d7
Anonna <meftahul@iut-dhaka.edu>
2026-06-21 10:51:37 +0600

    chore: rebuild to apply renamed repository path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+// Trigger build for repository rename to Anu.dev
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 
 const repo = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : ''
 const base = repo && repo !== 'github.io' && repo.includes('.github.io') ? '/' : repo ? `/${repo}/` : '/'
