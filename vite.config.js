975005a2c618656918465a198f88b0981a60425c
Anonna <meftahul@iut-dhaka.edu>
2026-06-21 10:52:58 +0600

    chore: add env logging to build

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 const repo = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : ''
 const base = repo && repo !== 'github.io' && repo.includes('.github.io') ? '/' : repo ? `/${repo}/` : '/'
+
+console.log('--- BUILD ENVIRONMENT DEBUG ---')
+console.log('GITHUB_REPOSITORY env:', process.env.GITHUB_REPOSITORY)
+console.log('Computed base path:', base)
+console.log('--------------------------------')
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: base,
 })
+
 
 
 
